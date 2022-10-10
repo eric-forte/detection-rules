40669faa3996dc863ae4d0fd06c07d4c7e707f0b
eric-forte <eric.forte@cire.tech>
2022-10-10 12:13:39 -0400

    Added diff package comparison

--- detection_rules/pkgcompare.py
+++ detection_rules/pkgcompare.py
@@ -4,6 +4,9 @@
 # 2.0.
 
 """CLI commands for internal detection_rules package comparison."""
+from dictdiffer import diff
+import json
+from pathlib import Path
 import requests
 
 import click
@@ -15,13 +18,21 @@ from .misc import client_error
 from .rule_loader import RuleCollection
 
 
-# NOTE this could go in utils.py if it uses a different error type
+# NOTE these two functions could go in utils.py if they use a different error type
 def get_content(url: str) -> bytes:
     try:
         r = requests.get(url)
         return r.content
     except requests.exceptions.RequestException as e:
         raise client_error(f"Requests error, unable to get zipfile. {e}")
+
+
+def get_json(content: bytes) -> dict:
+    try:
+        json_data = json.loads(content)
+        return json_data
+    except json.decoder.JSONDecodeError:
+        raise client_error("Unable to parse JSON")
 
 
 @root.group('pkgcompare')
@@ -31,9 +42,11 @@ def pkgcompare_group():
 
 @pkgcompare_group.command('compare-package')
 @click.option('--tag', required=True, type=str, help='Github repo tag to use for package comparison')
+@click.option('--cdn-url', '-u', default='https://epr.elastic.co/package/security_detection_engine/8.3.1/',
+              type=str, help='CDN URL to load rules package')
 @click.option('--repo', '-r', default='elastic/detection-rules', help='Override the elastic/detection-rules repo')
 @click.option('--skip-validation', '-s', is_flag=True, help='Bypass schema validation for Github repo rules')
-def compare_package(tag: str, repo: str, skip_validation: bool):
+def compare_package(tag: str, cdn_url: str, repo: str, skip_validation: bool):
     rules = RuleCollection()
     # Public repo, no token needed
     gh_client = GithubClient()
@@ -59,5 +72,32 @@ def compare_package(tag: str, repo: str, skip_validation: bool):
                 if skip_validation:
                     toml_dict['metadata']['query_schema_validation'] = False
                 rules.load_dict(toml_dict)
+
+    # Setup Diff dictionaries
+    # NOTE many options for how to populate/pre-populate tag_only this is more optimized than some
+    tag_only = {key: rules.id_map[key].contents.to_api_format() for key in rules.id_map.keys()}
+    cdn_only = {}
+    diffs = {}
+
+    base_content = get_json(get_content(cdn_url))
+    cdn_download_url = "https://epr.elastic.co" + base_content["download"]
+    package_version = base_content["version"]
+
+    # Get and Parse CDN rule package and diff against tagged rule package
+    with utils.unzip(get_content(cdn_download_url)) as archive:
+        base_path = f'security_detection_engine-{package_version}/kibana/security_rule/'
+        name_list = archive.namelist()
+        for file in name_list:
+            if(file.startswith(base_path) and file.endswith(".json")):
+                uuid = Path(file).stem
+                cdn_json = get_json(archive.open(file).read())
+                cdn_rule = cdn_json["attributes"]
+                if uuid not in rules.id_map.keys():
+                    cdn_only[uuid] = cdn_rule
+                else:
+                    result = list(diff(cdn_rule, tag_only[uuid]))
+                    if result:
+                        diffs[uuid] = result
+                    del tag_only[uuid]
 
     raise client_error("Not implemented.")
