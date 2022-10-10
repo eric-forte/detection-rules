c189c0f6a5dcda9f91c554a784764b6a3e602c36
eric-forte <eric.forte@cire.tech>
2022-10-10 10:48:41 -0400

    Get github tag zip url

--- detection_rules/pkgcompare.py
+++ detection_rules/pkgcompare.py
@@ -10,6 +10,8 @@ import click
 from .main import root
 from .misc import client_error
 
+from .ghwrap import GithubClient
+
 
 @root.group('pkgcompare')
 def pkgcompare_group():
@@ -18,5 +20,20 @@ def pkgcompare_group():
 
 @pkgcompare_group.command('compare-package')
 @click.option('--tag', required=True, type=str, help='Github repo tag to use for package comparison')
-def compare_package(tag: str):
+@click.option('--repo', '-r', default='elastic/detection-rules', help='Override the elastic/detection-rules repo')
+def compare_package(tag: str, repo: str):
+    # Public repo, no token needed
+    gh_client = GithubClient()
+    repo = gh_client.client.get_repo(repo)
+    tags = repo.get_tags()
+    git_download_url = None
+
+    matched_tag = [k for k in list(tags) if k.name == tag]
+    if matched_tag:
+        git_download_url = matched_tag[0].zipball_url
+    else:
+        raise client_error("Github tag not found.")
+
+    print(git_download_url)
+
     raise client_error("Not implemented.")
