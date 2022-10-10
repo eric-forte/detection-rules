a12aaae9988bd14c18b4a0d48af195b634d57e20
eric-forte <eric.forte@cire.tech>
2022-10-10 11:01:36 -0400

    Get repo tag package contents

--- detection_rules/pkgcompare.py
+++ detection_rules/pkgcompare.py
@@ -4,13 +4,24 @@
 # 2.0.
 
 """CLI commands for internal detection_rules package comparison."""
+import requests
 
 import click
 
+from . import utils
+from .ghwrap import GithubClient
 from .main import root
 from .misc import client_error
+from .rule_loader import RuleCollection
 
-from .ghwrap import GithubClient
+
+# NOTE this could go in utils.py if it uses a different error type
+def get_content(url: str) -> bytes:
+    try:
+        r = requests.get(url)
+        return r.content
+    except requests.exceptions.RequestException as e:
+        raise client_error(f"Requests error, unable to get zipfile. {e}")
 
 
 @root.group('pkgcompare')
@@ -21,7 +32,9 @@ def pkgcompare_group():
 @pkgcompare_group.command('compare-package')
 @click.option('--tag', required=True, type=str, help='Github repo tag to use for package comparison')
 @click.option('--repo', '-r', default='elastic/detection-rules', help='Override the elastic/detection-rules repo')
-def compare_package(tag: str, repo: str):
+@click.option('--skip-validation', '-s', is_flag=True, help='Bypass schema validation for Github repo rules')
+def compare_package(tag: str, repo: str, skip_validation: bool):
+    rules = RuleCollection()
     # Public repo, no token needed
     gh_client = GithubClient()
     repo = gh_client.client.get_repo(repo)
@@ -34,6 +47,17 @@ def compare_package(tag: str, repo: str):
     else:
         raise client_error("Github tag not found.")
 
-    print(git_download_url)
+    # Get and Parse rule packaged contents from repo tag
+    with utils.unzip(get_content(git_download_url)) as git_archive:
+        git_name_list = git_archive.namelist()
+        base_path = git_name_list[0] + "rules/"
+        # Get all rules
+        for file in git_name_list:
+            if(file.startswith(base_path) and file.endswith(".toml")):
+                # load valid rule file
+                toml_dict = rules.deserialize_toml_string(git_archive.open(file).read())
+                if skip_validation:
+                    toml_dict['metadata']['query_schema_validation'] = False
+                rules.load_dict(toml_dict)
 
     raise client_error("Not implemented.")
